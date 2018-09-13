89549029eac06ac394ee4d26dd82b1c2e10ae8f3
salisajat <43162315+salisajat@users.noreply.github.com>
2018-09-13 11:55:14 -0400

    using ClickDist & trig instead

--- sketch3/sk3.js
+++ sketch3/sk3.js
@@ -8,10 +8,17 @@ function draw(){
 }
 
 function mouseClicked(){ 
-	if(mouseX > width/2 -15 && mouseX < width/2 + 15){
-		if (mouseY > height/2 -15 && mouseY < height/2 + 15){
+	// if(mouseX > width/2 -15 && mouseX < width/2 + 15){
+	// 	if (mouseY > height/2 -15 && mouseY < height/2 + 15){
+	// 	fill(0); 
+	// 	} 
+	// } else { 
+	// 	fill(255); 
+	// }
+
+	var clickDist = dist(width/2, height/2, mouseX, mouseY); 
+	if (clickDist <15) { 
 		fill(0); 
-		} 
 	} else { 
 		fill(255); 
 	}
